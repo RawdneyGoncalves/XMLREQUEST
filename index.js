84fe71f7d3ede4417c3ab3373c2a8371c21e7749
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-01-17 19:16:47 -0300

    att

--- index.js
+++ index.js
@@ -2,24 +2,31 @@ const express = require("express");
 const ejs = require("ejs");
 const app = express();
 const cookieParser = require("cookie-parser");
+
+//middleware
 app.set("view engine", "ejs");
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/login", function (req, res) {
+// criando rota com funções de req, res
+
+app.get("/login", (req, res) => {
   res.render("index");
 });
 
-app.get("/inicio", function (req, res) {
+//
+app.get("/inicio", (req, res) => {
   res.render("inicio");
-  // console.log(inicio)
 });
-app.post("/inicio", function (req, res) {
+
+app.post("/inicio", (req, res) => {
   let token = req.body.token;
   console.log(` o token tá rodando aqui:  ${token}`);
-  
 });
-// configuracao
+
+// configuracao do servidor
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
