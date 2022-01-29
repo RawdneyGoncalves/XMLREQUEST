5552e16980eb435dfcf581422d7471a7f0f78712
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-01-29 12:49:05 -0300

    att

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-const express = require("express");
-const ejs = require("ejs");
+const express = require('express');
+const ejs = require('ejs');
 const app = express();
-const cookieParser = require("cookie-parser");
+const cookieParser = require('cookie-parser');
+
+const {OAuth2Client} = require('google-auth-library');
+CONST CLIENT_ID = ''
+const client = new OAuth2Client(CLIENT_ID);
 
 //middleware
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(cookieParser());
 
 // criando rota com funções de req, res
 
-app.get("/login", (req, res) => {
-  res.render("index");
+app.get('/login', (req, res) => {
+  res.render('index');
 });
 
 //
-app.get("/inicio", (req, res) => {
-  res.render("inicio");
+app.get('/inicio', (req, res) => {
+  res.render('inicio');
 });
 
-app.post("/inicio", (req, res) => {
+app.post('/inicio', (req, res) => {
   let token = req.body.token;
-  console.log(` o token tá rodando aqui:  ${token}`);
+  console.log(token);
 });
 
 // configuracao do servidor
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-  res.status(5000)
+  console.log(`Rodando na porta ${PORT}`);
 });
